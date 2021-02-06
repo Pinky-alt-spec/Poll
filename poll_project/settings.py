35ac0219c2c7486fa4bca528cb63d8315ce6bdf9
Pinky <janeletageng28@gmail.com>
2021-02-06 15:54:28 +0200

    pushing to git

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'poll',
     'widget_tweaks',
+    'poll',
 ]
 
 MIDDLEWARE = [
@@ -74,18 +74,12 @@ WSGI_APPLICATION = 'poll_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'poll',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'traditional',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
